88a9c9fb410e8552773d2ba63f43290bfaa368d8
Vaibhaw <kvaibhaw99@gmail.com>
2020-04-25 11:15:00 -0400

    Adding crossword.py

---
+++ crossword.py
@@ -0,0 +1,38 @@
+class Variable():
+
+    ACROSS = "across"
+    DOWN = "down"
+
+    def __init__(self, i, j, direction, length):
+        """Create a new variable with starting point, direction, and length."""
+        self.i = i
+        self.j = j
+        self.direction = direction
+        self.length = length
+        self.cells = []
+        for k in range(self.length):
+            self.cells.append(
+                (self.i + (k if self.direction == Variable.DOWN else 0),
+                 self.j + (k if self.direction == Variable.ACROSS else 0))
+            )
+
+    def __hash__(self):
+        return hash((self.i, self.j, self.direction, self.length))
+
+    def __eq__(self, other):
+        return (
+            (self.i == other.i) and
+            (self.j == other.j) and
+            (self.direction == other.direction) and
+            (self.length == other.length)
+        )
+
+    def __str__(self):
+        return f"({self.i}, {self.j}) {self.direction} : {self.length}"
+
+    def __hash__(self):
+        return hash((self.i, self.j, self.direction, self.length))
+
+    def __repr__(self):
+        direction = repr(self.direction)
+        return f"Variable({self.i}, {self.j}, {direction}, {self.length})"
