92f6969233d45d8ba924ce9aed7858e9fea8859a
Vaibhaw <kvaibhaw99@gmail.com>
2020-11-06 11:15:00 -0500

    Updating generator.py

--- generate.py
+++ generate.py
@@ -254,4 +254,30 @@ class CrosswordCreator():
         return None
 
 
+def main():
 
+    # Check usage
+    if len(sys.argv) not in [3, 4]:
+        sys.exit("Usage: python generate.py structure words [output]")
+
+    # Parse command-line arguments
+    structure = sys.argv[1]
+    words = sys.argv[2]
+    output = sys.argv[3] if len(sys.argv) == 4 else None
+
+    # Generate crossword
+    crossword = Crossword(structure, words)
+    creator = CrosswordCreator(crossword)
+    assignment = creator.solve()
+
+    # Print result
+    if assignment is None:
+        print("No solution.")
+    else:
+        creator.print(assignment)
+        if output:
+            creator.save(assignment, output)
+
+
+if __name__ == "__main__":
+    main()
