06920ae76bd7442af2adf1b9934ed5cb50451688
Vaibhaw <kvaibhaw99@gmail.com>
2020-11-30 11:15:00 -0500

    Updating generator.py

--- generate.py
+++ generate.py
@@ -27,7 +27,7 @@ class CrosswordCreator():
             direction = variable.direction
             for k in range(len(word)):
                 i = variable.i + (k if direction == Variable.DOWN else 0)
-                j = variable.j + (k if direction == Variable.ACROSS else 0)
+                j = variable.j + (k if direction == Variable.ACROSS else 0) 
                 letters[i][j] = word[k]
         return letters
 
